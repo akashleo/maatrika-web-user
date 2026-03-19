5040cb13703beda45b7eaa703b964e473df720df
Akash <akashleo2009@gmail.com>
2026-03-19 20:16:01 +0530

    chore: updated backend logic

--- backend/routes/transactionRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getAllTransactions, getTransactionById } from '../controllers/transactionController.js';
-
-const router = Router();
-
-router.get('/', getAllTransactions);
-router.get('/:id', getTransactionById);
-
-export default router;
